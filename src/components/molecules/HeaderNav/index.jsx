e28578befbbb45bc3e902c15444e62ac981ab42b
Juan Carlos Coral <govindasd@gmail.com>
2020-04-22 21:55:48 -0300

    Adding responsive styles for the logo and hamburger menu button on the header menu for mobile including extra small screens.

--- src/components/molecules/HeaderNav/index.jsx
+++ src/components/molecules/HeaderNav/index.jsx
@@ -37,7 +37,15 @@ const HeaderNav = () => (
             </a>
             <button className='navbar-toggler'>
                 <i-feather>
-
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        height="32px"
+                        viewBox="0 0 32 32"
+                        width="32px">
+                        <metadata id="metadata9">
+                        </metadata>
+                        <defs id="defs7" />
+                        <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" id="path2" />
+                    </svg>
                 </i-feather>
             </button>
             <NavList listInfo={listInfo} />
